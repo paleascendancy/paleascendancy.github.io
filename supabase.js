8ae08e426f5535ad3201b867626365935d4e20fb
paleascendancy <rarison1908@gmail.com>
2026-09-01 10:10:03 -0400

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://fnyellunugdfesprmvzm.supabase.co";
+
+const SUPABASE_KEY = "COLE_SUA_PUBLISHABLE_KEY_AQUI";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
