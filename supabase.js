7ad48939c10e4241272aa1ed52d353f788529951
paleascendancy <rarison1908@gmail.com>
2026-09-06 02:55:33 -0400

    chore: remove unused duplicate Supabase initializer

--- supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://fnyellunugdfesprmvzm.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_clf6HlhhxdftO1_XZU7YsA_pRmkCEJK";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
